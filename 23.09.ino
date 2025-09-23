9317987a94b08211b6a45e20f0e4aaf207c189e1
mariamaed29 <maed.eduarda2009@gmail.com>
2025-09-23 16:41:50 -0300

    Add files via upload

---
+++ 23.09.ino
@@ -0,0 +1,24 @@
+#define led1 9
+#define led2 10
+#define led3 11
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+   pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  delay(1000);
+
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+   digitalWrite(led3, LOW);
+  delay(1000);
+}
+
